71417e90b7145529fddc4a609d629855da1aed1d
Marco Carmosino <mcarmosi@cs.ucsd.edu>
2014-04-29 01:23:50 -0700

    Added runscript for part 3

--- runscripts/run-part2.sh
+++ runscripts/run-part2.sh
@@ -19,19 +19,19 @@ BENCH_BC="$BENCH_NAME.bc"
 
 echo $BENCH_BC
 
-opt -load $LLVMLIB/CSE231.so -CountDynamicInstructions < "$1/$BENCH_BC" > $BENCH_NAME.instrumented.bc
+opt -load $LLVMLIB/CSE231.so -CountDynamicInstructions < "$1/$BENCH_BC" > $BENCH_NAME.dynamicinstrumented.bc
 
 ## compile the instrumentation module to bitcode
 clang $CPPFLAGS -O0 -emit-llvm -c $OUR_INSTRUMENTS/dynamic/dynamicinstr.cpp -o $BENCH_NAME.dynamicinstr.bc
 
-# ## link instrumentation module
-llvm-link $BENCH_NAME.instrumented.bc $BENCH_NAME.dynamicinstr.bc -o $BENCH_NAME.linked.bc
+## link instrumentation module
+llvm-link $BENCH_NAME.dynamicinstrumented.bc $BENCH_NAME.dynamicinstr.bc -o $BENCH_NAME.dynamiclinked.bc
 
-# ## compile to native object file
-llc -filetype=obj $BENCH_NAME.linked.bc -o=$BENCH_NAME.o
+## compile to native object file
+llc -filetype=obj $BENCH_NAME.dynamiclinked.bc -o=$BENCH_NAME.o
 
 # ## generate native executable
-g++ $BENCH_NAME.o $LDFLAGS -o $BENCH_NAME.instrumented.exe
+g++ $BENCH_NAME.o $LDFLAGS -o $BENCH_NAME.dynamicinstrumented.exe
 
 ./$BENCH_NAME.instrumented.exe
 
