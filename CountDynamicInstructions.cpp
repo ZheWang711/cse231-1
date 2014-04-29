3747eaafa005cdb1cf60fa8899b5295e825d718a
Marco Carmosino <marco@death-kitten>
2014-04-28 17:42:28 -0700

    Works on welcome benchmark.

--- CountDynamicInstructions.cpp
+++ CountDynamicInstructions.cpp
@@ -21,7 +21,7 @@ namespace {
     // This is the main body of our code.
     virtual bool runOnModule(Module &M) {
     
-	// Function definition for void function (int)
+	// Function definition for void countInstExecuted(int)
 	// Mangled Name of instrumentation function: _Z17countInstExecutedi
 	std::vector<Type*> ConstCharPtrTy2_args;
 
@@ -34,9 +34,21 @@ namespace {
 	Constant* c2 = M.getOrInsertFunction("_Z17countInstExecutedi", FuncTy2);
 	Function* countInstFunc = dyn_cast<Function>(c2);
   
+	// Function definition for void printEverything()
+	// Mangled Name of instrumentation function: _Z15printEverythingv
+	std::vector<Type*> VoidTy_args;
+	FunctionType* VoidTy = FunctionType::get(
+						 /*Result=*/Type::getVoidTy(M.getContext()),
+						 /*Params=*/VoidTy_args,
+						 /*isVarArg=*/false);
+	Constant* c3 = M.getOrInsertFunction("_Z15printEverythingv", VoidTy);
+	Function* printFunc = dyn_cast<Function>(c3);
+	
+
 	// Create the IRBuilder
 	IRBuilder<> builder(M.getContext());
-
+	bool printed = false;
+    
 	for (Module::iterator MI = M.begin(), ME = M.end(); MI != ME; ++MI){
 	  // MI is an iterator over functions in the program.
 	  for (inst_iterator I = inst_begin(MI), E = inst_end(MI); I != E; ++I){
@@ -50,6 +62,13 @@ namespace {
 
 	    builder.SetInsertPoint(&*I);
 	    builder.CreateCall(countInstFunc, OpcodeValue);
+	  }
+	  if (MI->getName() == "main") {
+	    // This is a little hacky -- only the getPrevNode() and getNextNode() methods
+	    // return the correct type. Fix this if you can.
+	    builder.SetInsertPoint(MI->back().getInstList().back().getPrevNode()->getNextNode());
+	    builder.CreateCall(printFunc);
+	    // printed = true; // only call once	  	  
 	  }
 	}
 	return false;
