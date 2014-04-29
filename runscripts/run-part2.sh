bbbd9420d1ac4bd4c1979528d9f4cc531c048ec4
Marco Carmosino <mcarmosi@cs.ucsd.edu>
2014-04-29 01:10:40 -0700

    Finished "dynamic" runscript, added logs for dynamic pass

--- runscripts/run-part2.sh
+++ runscripts/run-part2.sh
@@ -11,25 +11,27 @@ OUR_INSTRUMENTS="$CSE231ROOT/llvm/build/lib/CSE231/instrumentation/"
 
 BENCH_NAME=`basename $1`
 
+cd $1
+
 echo $BENCH_NAME
 
 BENCH_BC="$BENCH_NAME.bc"
 
 echo $BENCH_BC
 
-opt -load $LLVMLIB/CSE231.so -CountDynamicInstructions < "$1/$BENCH_BC" > $INSTRUMENTATION/dynamic/$BENCH_NAME.instrumented.bc
+opt -load $LLVMLIB/CSE231.so -CountDynamicInstructions < "$1/$BENCH_BC" > $BENCH_NAME.instrumented.bc
 
 ## compile the instrumentation module to bitcode
-clang $CPPFLAGS -O0 -emit-llvm -c $OUR_INSTRUMENTS/dynamic/dynamicinstr.cpp -o $OUR_INSTRUMENTS/dynamic/dynamicinstr.bc
+clang $CPPFLAGS -O0 -emit-llvm -c $OUR_INSTRUMENTS/dynamic/dynamicinstr.cpp -o $BENCH_NAME.dynamicinstr.bc
 
 # ## link instrumentation module
-llvm-link $INSTRUMENTATION/dynamic/$BENCH_NAME.instrumented.bc $OUR_INSTRUMENTS/dynamic/dynamicinstr.bc -o $BENCH_NAME.linked.bc
+llvm-link $BENCH_NAME.instrumented.bc $BENCH_NAME.dynamicinstr.bc -o $BENCH_NAME.linked.bc
 
 # ## compile to native object file
-# llc -filetype=obj gcd.linked.bc -o=gcd.o
+llc -filetype=obj $BENCH_NAME.linked.bc -o=$BENCH_NAME.o
 
 # ## generate native executable
-# g++ gcd.o $LDFLAGS -o gcd.exe
+g++ $BENCH_NAME.o $LDFLAGS -o $BENCH_NAME.instrumented.exe
 
-# ./gcd.exe
+./$BENCH_NAME.instrumented.exe
 
