2e1957d93f1e9e8d7bf6146ec640d5ce59443ae9
Marco Carmosino <mcarmosi@cs.ucsd.edu>
2014-04-29 01:30:51 -0700

    added runscript for part 1

---
+++ runscripts/run-part1.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+BENCH_NAME=`basename $1`
+
+cd $1
+
+echo $BENCH_NAME
+
+BENCH_BC="$BENCH_NAME.bc"
+
+echo $BENCH_BC
+
+opt -analyze -load $LLVMLIB/CSE231.so -CountStaticInstructions < $BENCH_BC
