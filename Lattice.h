3f468971d95c44391192e8d4826b7a957dfa7ee0
Christine Luu <cluu908@gmail.com>
2014-05-22 10:27:28 -0700

    Lattice abstract classes

---
+++ Lattice.h
@@ -0,0 +1,18 @@
+#ifndef LATTICE_H_
+#define LATTICE_H_
+
+#include "LatticePoint.h"
+
+class Lattice {
+public:
+  Lattice();
+  virtual ~Lattice();
+  virtual LatticePoint join(LatticePoint in1, LatticePoint in2);
+
+private:
+  LatticePoint top;
+  LatticePoint bottom;
+};
+
+#endif /* LATTICE_H_ */
+
---
+++ LatticePoint.h
@@ -0,0 +1,22 @@
+#ifndef LATTICEPOINT_H_
+#define LATTICEPOINT_H_
+
+#include "llvm/IR/Module.h"
+#include "llvm/IR/Function.h"
+#include "llvm/IR/Instructions.h"
+#include "llvm/Support/InstIterator.h"
+#include <map>
+
+class LatticePoint {
+public:
+  LatticePoint();
+  LatticePoint(llvm::Instruction instr, LatticePoint point);
+  virtual ~LatticePoint();
+
+private:
+  std::map<int, const LatticePoint *> instrMap;
+  virtual bool equality (LatticePoint in1, LatticePoint in2);
+};
+
+#endif /* LATTICEPOINT_H_ */
+
