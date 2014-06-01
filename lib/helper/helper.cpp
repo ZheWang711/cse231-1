6caa1d77ccd86c30eb692e688099031b57f5886c
ctosh@cs.ucsd.edu <ctosh@cs.ucsd.edu>
2014-05-31 18:23:29 -0700

    maybe fixed

--- lib/helper/helper.cpp
+++ lib/helper/helper.cpp
@@ -1,6 +1,6 @@
 #include "helper.h"
 
-ConstantInt* helper::foldBinaryOperator(unsigned Opcode,ConstantInt *C1, ConstantInt *C2){
+static ConstantInt* helper::foldBinaryOperator(unsigned Opcode,ConstantInt *C1, ConstantInt *C2){
   LLVMContext &context = C1->getContext();
   const APInt &c1_value = C1->getValue();
   const APInt &c2_value = C2->getValue();
