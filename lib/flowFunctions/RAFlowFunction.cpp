1ec853c10f3dca85a21dbdf04012d39c6045bacd
ctosh@cs.ucsd.edu <ctosh@cs.ucsd.edu>
2014-06-01 12:45:13 -0700

    trying to debug

--- lib/flowFunctions/RAFlowFunction.cpp
+++ lib/flowFunctions/RAFlowFunction.cpp
@@ -5,9 +5,10 @@
 
 LatticePoint RAFlowFunction::operator()(llvm::Instruction* instr, std::vector<LatticePoint *> info_in){
   // dyncast on that vector;
-  info_in_casted = std::vector<RALatticePoint *>;
+  info_in_casted = std::vector<RALatticePoint *>();
   for (std::vector<LatticePoint *>::iterator it = info_in.begin(); it != info_in.end(); ++it){
-    info_in_casted.push_back(dyn_cast<RALatticePoint>(&*it));
+    RALatticePoint* temp = dyn_cast<RALatticePoint>(*it)
+    info_in_casted.push_back(temp);
   }
   this->visit(instr);
   return ret_value;
