3b7ebb8bbac42a777b9efcdf3d84d8ed7bb6b17d
ctosh@cs.ucsd.edu <ctosh@cs.ucsd.edu>
2014-06-01 18:30:18 -0700

    doing what marco told me to do

--- lib/flowFunctions/FlowFunction.cpp
+++ lib/flowFunctions/FlowFunction.cpp
@@ -2,13 +2,13 @@
 #include "llvm/IR/Module.h"
 #include "llvm/IR/Function.h"
 #include "llvm/IR/Instructions.h"
-
+#include "llvm/Support/raw_ostream.h"
 #include <functional>
 #include <iostream>
 
 std::vector<LatticePoint *> FlowFunction::operator()(llvm::Instruction* instr, std::vector<LatticePoint*> info_in){
   CPLatticePoint lp;
-  
+  errs() << "I told you so, Marco \n";
   std::vector<LatticePoint*> info_out;
   info_out.push_back(&lp);
   return info_out;
