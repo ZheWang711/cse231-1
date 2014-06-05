71cae4ba47cb0a510db9c4392bef01cbdf100eec
ctosh@cs.ucsd.edu <ctosh@cs.ucsd.edu>
2014-06-05 16:19:08 -0700

    more pointer work

--- examples/pointer/pointer.cpp
+++ examples/pointer/pointer.cpp
@@ -1,10 +1,12 @@
 int f(int x){
   
   // Purely local
-  int a = 10;
-  int* b = &a;
-  *b = 5;
-  int c = *b;
+  int a_1 = 10;
+  int a_2 = 15;
+  int* b_1 = &a_1;   // b_1 --> a_1
+  int* b_2 = &a_2;   // b_1 --> a_1, b_2 --> a_2
+  int** c = &b_1;    // b_1 --> a_1, b_2 --> a_2, c --> b_1
+  *c = b_2;          // b_1 --> a_1, b_1 --> a_2, b_2 --> a_2, c --> b_1
   
   
   // Non-local
