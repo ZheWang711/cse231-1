dee537434c13ab6ee31ff49f0366a5a47ede4370
ctosh@cs.ucsd.edu <ctosh@cs.ucsd.edu>
2014-06-09 14:44:17 -0700

    Added range checking pass. Testing now

---
+++ lib/projectOne/RangeCheckingPass.cpp
@@ -0,0 +1,82 @@
+#include "llvm/Pass.h"
+#include "llvm/IR/InstrTypes.h"
+#include "llvm/IR/Module.h"
+#include "llvm/IR/Function.h"
+#include "llvm/Support/raw_ostream.h"
+#include <iostream>
+#include "llvm/Support/InstIterator.h"
+#include "lattices/RALatticePoint.h"
+#include "flowFunctions/RAFlowFunction.h"
+#include "Analysis.h"
+#include <map>
+#include <list>
+#include <utility>
+#include <stack>
+#include <stdlib.h>
+#include "helper/helper.h"
+
+using namespace llvm;
+
+
+struct RangeCheckingPass : public FunctionPass {
+  static char ID;
+  
+  
+  RangeCheckingPass() : FunctionPass(ID) {}
+  
+  // This is the main body of our code.
+  virtual bool runOnFunction(Function &F){
+    errs() << " -----Starting Range Function Checking Pass------ \n";
+    
+    RAFlowFunction raf = RAFlowFunction();
+    
+    std::map<Value*, ConstantRange*> representation;
+    
+    /*
+    errs() << "We are dealing with the following function \n";
+    F.print(errs());
+    errs() << "Our analysis returned the following \n";
+    */
+    
+    RALatticePoint* bottom = new RALatticePoint(true, false, representation);
+    std::map<Instruction *, LatticePoint *> result = Analysis::analyze(F, bottom, &raf);
+    
+    int i = 1;
+    bool has_range_warnings = false;
+    for (inst_iterator I = inst_begin(F), E = inst_end(F); I != E; ++I){
+      
+      Value* current_instruction = &*I;
+      LatticePoint* lp = result[current_instruction];
+      RALatticePoint *rlp = dyn_cast<RALatticePoint>(lp);
+      
+      if (isa<GetElementPtrInst>(current_instruction)) {
+        GetElementPtrInst* gep = cast<GetElementPtrInst>(current_instruction);
+        errs() << "GEP instruction: ";
+        gep->print(errs());
+        errs() << " has " << gep->getNumIndices() << " indices \n";
+      }
+      
+      I->print(errs());
+      errs() << " --> ";
+      rlp->printToErrs();
+      i++;
+    }
+    
+    if (has_range_warnings) {
+      errs() << "Done with range checking. Exited with errors!\n\n"
+    }
+    else{
+      errs() << "Done with range checking. Exited with no errors.\n\n"
+
+    }
+    
+    errs() << " -----Ending Range Function Checking Pass------ \n";
+    
+    return false;
+  }
+  
+};
+//}
+
+char RATest::ID = 0;
+static RegisterPass<RangeCheckingPass> X("RangeCheckingPass", "RangeCheckingPass Pass", false, false);
